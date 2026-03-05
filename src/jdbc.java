bcc7f79bc080b8cdb5d4b2ac049616bcb04c8db4
TANISH <shahtanish18@gamil.com>
2026-03-05 18:53:47 +0530

    jdbc done

---
+++ src/jdbc.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public class jdbc{
+    public static Connection getOracleConnection() throws Exception {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        return DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:XE",
+                "system",
+                "Tanish2006#");
+    }
+}
