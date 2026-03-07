ea6a22945c768062e2886cf48cf6bc5d5e15722d
TANISH <shahtanish18@gamil.com>
2026-03-07 10:11:49 +0530

    customer , order details,products class added

---
+++ src/order_details.java
@@ -0,0 +1,65 @@
+public class order_details {
+    int order_id;
+    int cust_id;
+    String order_date;
+    int total;
+    int tax;
+    int discount;
+    int final_bill;
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getCust_id() {
+        return cust_id;
+    }
+
+    public void setCust_id(int cust_id) {
+        this.cust_id = cust_id;
+    }
+
+    public String getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(String order_date) {
+        this.order_date = order_date;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getTax() {
+        return tax;
+    }
+
+    public void setTax(int tax) {
+        this.tax = tax;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public int getFinal_bill() {
+        return final_bill;
+    }
+
+    public void setFinal_bill(int final_bill) {
+        this.final_bill = final_bill;
+    }
+}
---
+++ src/product.java
@@ -0,0 +1,29 @@
+public class product {
+    int pro_id;
+    int pro_name;
+    int pro_qty;
+
+    public int getPro_id() {
+        return pro_id;
+    }
+
+    public void setPro_id(int pro_id) {
+        this.pro_id = pro_id;
+    }
+
+    public int getPro_name() {
+        return pro_name;
+    }
+
+    public void setPro_name(int pro_name) {
+        this.pro_name = pro_name;
+    }
+
+    public int getPro_qty() {
+        return pro_qty;
+    }
+
+    public void setPro_qty(int pro_qty) {
+        this.pro_qty = pro_qty;
+    }
+}
